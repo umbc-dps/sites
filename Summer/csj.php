73753d76d3d34bf284112ab338759d598759407f
umbc-dps <jgilless123@gmail.com>
2016-02-10 13:28:29 -0500

    Added copy to Culture and Social Justice page.

--- Summer/csj.php
+++ Summer/csj.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 <head>
+
 <meta charset="UTF-8">
 <title>Culture & Social Justice | Summer Programs at UMBC</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -69,74 +70,247 @@
   <div class="narrowchart">
  <div id="accordion">   
    <!-- Start Content --> 
+ <h5>Social Justice (so·cial jus·tice) noun. 1: a social philosophy advocating the removal of inequalities among people; 2: the idea of acting with equality, fairness and dignity to all human beings.</h5>  <br>
+
+   
  <p>This summer, interdisciplinary departments in UMBC’s College of Arts, Humanities, and Social Sciences will offer general education courses focused on topics in culture and social justice.</p>
  
+ <p>These courses address relevant social issues, while promoting critical thinking and writing skills. They offer a view of social issues and practices from various perspectives, as well as the intersectional analysis of race, class, and gender. Courses in culture & social justice invite students to understand and analyze structural inequalities while challenging them to envision and build a better future.</p>
+  
  <h3>Africana Studies</h3>
  
-<div class="accordionheader">
-  <h4>What is <em>my</em>UMBC?<i class="fa fa-angle-down"></i></h4>
-</div>
-
- <div class="accordionbody">
-<ul>
-<li>A web-based portal to various services and online community resources at UMBC.</li>
-<li>Use <a href="http://my.umbc.edu/"><em>my</em>UMBC</a> to register for courses, view account balances, online bill payment, register for a parking permit, check university e-mail and check grades.</li>
-</ul>
-</div>  
-  
-</li>
-<div class="accordionheader">
-  <h4>How do I create a <em>my</em>UMBC account?<i class="fa fa-angle-down"></i></h4>
-</div>
-
- <div class="accordionbody">
-<ul>
- <li>Create your account with your date of birth and UMBC Campus ID (which you will receive from the Admissions Office).</li>
- <li>Visit the <a href="https://webadmin.umbc.edu/admin"><em>my</em>UMBC WebAdmin page</a>.</li>
- <li>Click on <a href="https://webadmin.umbc.edu/admin/User/Create">Create my Account</a>.</li>
-</ul>
- </div> 
-   
-<div class="accordionheader">
-  <h4>How do I forward my UMBC e-mail to another e-mail account?<i class="fa fa-angle-down"></i></h4>
-</div>
-
- <div class="accordionbody">
-  <p>View how to <a href="https://wiki.umbc.edu/pages/viewpage.action?pageId=33851010">forward</a> your UMBC e-mails to an already existing e-mail account (Hotmail, Gmail, etc.).</p>
-</div>
-<div class="accordionheader">
-  <h4>Where can I eat on campus during summer &amp; winter session?<i class="fa fa-angle-down"></i></h4>
-</div>
-
- <div class="accordionbody">
-  <ul>
-  <li>On-campus dining is available summer &amp; winter session. <a href="http://www.dineoncampus.com/umbc/show.cfm?cmd=_summerSession">UMBC Dining Services</a> will post open venues and hours closer to the start of the session.</li>
-  <li><a href="http://www.dineoncampus.com/umbc/show.cfm?cmd=_summerSession">Meal Plans</a> are also available for Summer Session.</li></ul>
-</div>
+ <p><em>Africana Studies provides the tools to understand, critically evaluate, analyze and interpret events and phenomena that structure the experiences, possibilities and dynamics of the people of African descent in the U.S., Africa and its Diasporas. Courses address the cultural, economic, historical, political, psychological and sociological issues that affect peoples of African descent.</em></p>
  
 <div class="accordionheader">
-  <h4>How can I get help, when I need it?<i class="fa fa-angle-down"></i></h4>
+  <h4>AFST 100: Introduction to the Black Experience<i class="fa fa-angle-down"></i></h4>
 </div>
 
  <div class="accordionbody">
-<ul>
-<li>Submit an online <a href="faq"><em>my</em></a> <a href="https://rt.umbc.edu/UMBC/RequestHelp.html">UMBC Help Ticket</a>. Use myUMBC Help Tickets for billing issues, computing/technology support, questions about your class schedule, grades, etc.</li>
-<li>Access the myUMBC Help Ticket site and login with your myUMBC user name and password.</li>
-<li>Additional administrative support information can be found <a href="deptcontact.php">here</a>.</li>
-</ul>
-</div>   
+ <p>Introduces the black experience in the African Diaspora through a survey of historical and sociocultural ties that link people of African descent worldwide. Evaluates ideas about the different cultural, economic, psychological, sociological, and political challenges confronting people of African descent in the 21st century. Particular attention is paid to how to evaluate those challenges and proposed solutions in the aftermath of Obama’s presidency.</p>
+
+<p><strong>GEP: (AH)<br>
+4W1 – ONLINE<br>
+Tyson King-Meadows</strong></p>
+ </div>  
+  
+<div class="accordionheader">
+  <h4>AFST 213: Africa: Culture & Development<i class="fa fa-angle-down"></i></h4>
+</div>
+
+ <div class="accordionbody">
+<p>An introduction to the history, peoples, cultures, languages, and societies of the African continent. While addressing the diversity of African cultural history and development, other themes include the colonial and post-colonial period, African nationalism, neo-colonialism, and African culture and development today.</p>
+
+<p>
+<strong>GEP: (AH) or (C)<br>
+4W1 – Mon/Wed/Fri 9-12:30<br>
+Gloria Chuku</strong></p>
+ </div>
+ 
+  
+<div class="accordionheader">
+  <h4>AFST 370: Black Women: Cross-Cultural Perspectives<i class="fa fa-angle-down"></i></h4>
+</div>
+
+ <div class="accordionbody">
+  <p> A comparative examination of selected social, cultural, economic, and psychological factors that influence the lives of black women in Africa and the Diaspora. Particular attention is paid to the contemporary circumstances impacting the life chances of black American women.  Also listed as GWST 370. </p>
+  
+  <p> <strong> GEP: (C) or (SS) <br>
+4W2 –Tues/Thurs/Fri 1 – 4:10 pm HYBRID <br>
+Tammy Henderson</strong></p> 
+</div>
+
+<br>
+<hr>
+
+ <h3>American Studies</h3>
+ <p> <em>A focus on issues of race, ethnicity, class, gender, and social justice has always been integral to the American Studies Department at UMBC. These courses provide skills to critically analyze issues of place, identity, and community through a comparative and transnational lens. </em></p>
+ 
+<div class="accordionheader">
+  <h4>AMST 320: TV and American Culture <i class="fa fa-angle-down"></i></h4>
+</div>
+
+ <div class="accordionbody">
+<p>Examines the economic, social and artistic implications of television in the United States. It explores how TV’s history interacts with class, gender, ethnicity, and sexuality in terms of social hegemony. Explore new ways people consume media and how television is now available on many kinds of platforms and what that means for the industry as it tries to survive in the 21st century. </p>  
+
+<p> <strong> GEP: (AH)<br>
+  6W1 – Tues/Thurs 1 - 4:10 pm <br>
+  Michael Hummel</strong> </p>
+</div>
+  
+  
+<div class="accordionheader">
+  <h4>AMST 352: American Culture in Global Perspective <i class="fa fa-angle-down"></i></h4>
+</div>
+
+ <div class="accordionbody">
+<p> Emphasizes American consumer culture in the context of globalization. Illuminates the meaning and history of particular American social structures, cultural values and ideological themes by placing them in a global perspective. Explore topics like "McDonaldization" as well as the global labor and ecological issues surrounding the garment industry, and the representational politics of American mass-mediated entertainment as public diplomacy. </p>
+ 
+ <p> <strong> GEP: (AH) or (C)<br>
+ 6W1 - Tues/Thurs 1 - 4:10 pm (SHADY GROVE) <br>
+Dabrina Taylor</strong></p>
+</div>
 
 <div class="accordionheader">
-  <h4>Is on-campus housing available?<i class="fa fa-angle-down"></i></h4>
+  <h4>AMST 375: Studies in Asian American Culture <i class="fa fa-angle-down"></i></h4>
 </div>
 
  <div class="accordionbody">
-<ul>
-  <li>Students living in continuous occupancy, 9-month housing may remain on campus during January for Winter Session.  </li>
-  <li><a href="housing" target="_blank">Click here</a> for more information.</li>
-</ul>
+<p>Surveys salient topics and debates in Asian American cultural studies. Examines historical information on patterns of immigration for various Asian immigrant groups, but the main focus explores cultural contributions of Asians in the U.S. through literature and film. Explores the role of cultural expression and artistic representation in Asian American attempts to establish a sense of belonging and cultural presence in the U.S.  </p>  
+<p> 
+<strong> GEP: (AH) GEP: (AH) or (C) <br>
+6W2 - Tues/Thurs 6 - 9:10 pm <br>
+Neha Singhal</strong></p>
 </div>
+  
+<div class="accordionheader">
+  <h4>AMST 395: American Music and Culture <i class="fa fa-angle-down"></i></h4>
+</div>
+
+ <div class="accordionbody">
+<p>Surveys salient topics and debates in Asian American cultural studies. Examines historical information on patterns of immigration for various Asian immigrant groups, but the main focus explores cultural contributions of Asians in the U.S. through literature and film. Explores the role of cultural expression and artistic representation in Asian American attempts to establish a sense of belonging and cultural presence in the U.S.  </p>  
+
+<p> <strong> GEP: (AH) GEP: (AH) or (C) <br>
+6W2 - Tues/Thurs 6 - 9:10 pm <br>
+Neha Singhal</strong></p>
+ </div>
+ <br>
+<hr>
+ 
+  <h3> Anthropology</h3>
+  
+  <p> <em>Anthropology is the worldwide comparative study of humankind, present and past. UMBC’s Cultural Anthropology program promotes deeper understanding of the diversity of cultural worlds, human social organization and social structure, and social research methods. Anthropology courses help students understand the complexity of current events within the U.S. and internationally as well as the importance of recognizing and weighing equally cultural values and ideas from across the globe. </em></p>
+  
+    
+<div class="accordionheader">
+  <h4> ANTH 211: Cultural Anthropology <i class="fa fa-angle-down"></i></h4>
+</div>
+
+ <div class="accordionbody">
+<p>An introduction to the central concepts and issues in cultural anthropology. Employs an ethnographic perspective to examine topics including the concept of culture and diversity; social organization, exchange systems, and power within and across societies; religion, rights, and other value systems; and the possibilities for agency and change.  </p>  
+
+<p> <strong> GEP: (C) or (SS) <br>
+6W1 – Tues/Thurs 1 - 4:10 pm <br>
+Sarah Chard </strong> </p>
+
+<p> <strong> 6W2 – Tues/Thurs 6 - 9:10 pm <br>
+Bambi Chapin</strong> </p>
+</div>
+
+<div class="accordionheader">
+ <h4> ANTH 326: American Indian Cultures <i class="fa fa-angle-down"></i></h4>
+</div>
+
+<div class="accordionbody">
+<p>Drawing on contemporary ethnographic studies, ANTH 326 explores the cultural diversity of groups indigenous to the Americas. It examines issues of representation and identity in contemporary North American Indian communities including the role of anthropological interpretation and its impact on perceptions of Native America. Emphasizes the economic and political adaptations made by Indigenous people through the early 21st century in response to social change and challenges to social justice. </p>
+
+<p> <strong>GEP: (C) or (SS) <br>
+6W2 – Wed 1 – 4:10 pm HYBRID <br>
+Margaret Knisley </strong></p>
+</div>
+
 <br>
+<hr>
+
+<h3> Gender & Women’s Studies</h3>
+<p> <em>Gender and Women’s Studies courses provide skills to critically analyze how gender and sexuality intersect with differences of race, class, nationality, ability status, and more to make our world. Courses help demonstrate how these axes of difference structure unequal access to resources, distribute life chances differently, and both produce and challenge justice in local and global settings. GWST courses help students think critically, not just about the history and origins of injustice, but also the ways people continue to strive to achieve social justice.</em></p>
+
+<div class="accordionheader">
+ <h4> GWST 210: Introduction to Critical Sexuality Studies<i class="fa fa-angle-down"></i></h4>
+</div>
+
+<div class="accordionbody">
+<p>Examines the history and roots of inequality with a particular focus on sexual and gender minorities both locally and globally. It also looks at the intersections of sexuality with other differences such as gender, race, class, and ability. Students learn about social justice movements to challenge these inequalities. Also listed as AMST 210. </p>
+
+<p> <strong>GGEP: (SS) or (C) <br>
+6W1 - Wed 1-4:10 pm HYBRID <br>
+Kate Drabinski</strong></p>
+</div>
+
+<div class="accordionheader">
+ <h4> GWST 310: Gender and Inequality in America<i class="fa fa-angle-down"></i></h4>
+</div>
+
+<div class="accordionbody">
+<p>Examines the roots of gender, class, and race-based inequalities through case studies of gender, labor,and globalization and the growth of the U.S. prison system. Students are encouraged not just to understand these issues, but also learn to evaluate and propose policy solutions and actions for social 
+change. Also listed as AMST 310. </p>
+
+<p> <strong>GEP: (AH) <br>
+6W1 - Mon 1-4:10 pm HYBRID <br>
+Kate Drabinski</strong></p>
+</div>
+
+
+<div class="accordionheader">
+ <h4> GWST 338/POLI 338: Women, Gender and Law <i class="fa fa-angle-down"></i></h4>
+</div>
+
+<div class="accordionbody">
+<p>Examines ways in which gender affects rights within the American civil and criminal legal systems. It explores the interrelationship between traditional attitudes and stereotypes concerning women's roles in society and the historical development of women's legal rights. Topics include discrimination in employment and education, reproduction rights, and criminal law issues affecting women, including domestic violence, rape and prostitution. </p>
+
+
+<p> <strong>GEP:(SS) <br>
+6W2 – Mon/Wed 6:30 – 9:40 pm <br>
+Jennifer Kent</strong></p>
+</div>
+
+<br>
+<hr>
+
+<h3>Sociology</h3>
+<p> <em>Focuses on race and ethnic inequality from a sociological perspective by examining sociocultural, economic, and political factors that have shaped contemporary race and ethnic relations in the U.S. Issues addressed include power, privilege, racism, immigration, discrimination, and social change based on resistance and social movements.</em></p>
+
+<div class="accordionheader">
+ <h4>SOCY 201:  Social Problems in American Society<i class="fa fa-angle-down"></i></h4>
+</div>
+
+<div class="accordionbody">
+<p>Examines the key social problems in contemporary U.S. society from a sociological perspective. Social issues include poverty, crime and violence, gender and race discrimination, and unequal access to societal resources, such as education and healthcare. Learn to think creatively about possible solutions to social problems by understanding who is involved in defining social problems and the roles played by movements in raising awareness about social issues.</p>
+
+<p> <strong>GEP: (SS) <br>
+6W2 – ONLINE<br>
+Margaret Knisley
+</strong></p>
+</div>
+
+<div class="accordionheader">
+ <h4>SOCY 321: Race and Ethnic Relations<i class="fa fa-angle-down"></i></h4>
+</div>
+
+<div class="accordionbody">
+<p>Focuses on race and ethnic inequality from a sociological perspective by examining sociocultural, economic, and political factors that have shaped contemporary race and ethnic relations in the U.S. Issues addressed include power, privilege, racism, immigration, discrimination, and social change based on resistance and social movements.</p>
+
+<p> <strong>
+GEP: (SS)<br>
+6W1 - Tues/Thurs 6-9:10 pm<br>
+Karon Phillips
+</strong></p>
+</div>
+
+<div class="accordionheader">
+ <h4>SOCY/LLC 606: Social Inequality and Social Policy<i class="fa fa-angle-down"></i></h4>
+</div>
+
+<div class="accordionbody">
+<p>Examines sociological approaches to studying ethnic, class, and gender inequalities, and the social policies designed to address these inequalities in historical and comparative context. It provides conceptual and analytical tools to go beyond popular mainstream ideas about the structure and dynamics of social inequality. Critically analyze policy relevant sets of concepts, such as individualism and collectivism, domination and exploitation, social citizenship and responsibility, and social justice.</p>
+
+<p> <strong>
+6W1 – Tues/Thurs 6-9:10 pm<br>
+Marina Adler
+</strong></p>
+</div>
+
+<br>
+<hr>
+
+<h3>Summer Service Learning</h3>
+<p>You can make a difference!  Summer is a great time to make a meaningful contribution in your community through service learning. UMBC’s Shriver Center coordinates summer service learning placements in a variety of areas, including education, the environment, food and housing access, healthcare and mental health. </p>
+
+<p>Develop leadership skills, build self-confidence, and help create a better future. Change begins with you!</p>
+
+<p>Contact The Shriver Center at 410-455-2493, or visit <a href="http://shrivercenter.umbc.edu/service-learning/" target="_blank">The Shriver Center</a> for details.</p>
+
+ <br>
+
      <!-- END Content -->
      
 	</div></div></div>
@@ -150,12 +324,10 @@
     <!-- /five columns omega --> 
     <!-- END TOP SIDEBAR-->
   </div>
-
-  <footer id="site-footer" class="clearfix">
+<footer id="site-footer" class="clearfix">
     <?php include("includes/footer.php"); ?>
     <?php include("includes/tracking.php"); ?>
-  </footer>
-</div>
+</footer> 
 <!-- container --> 
 
 <script src="//code.jquery.com/jquery-1.12.0.js"></script> 
