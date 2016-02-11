6ea98d19a7a262551ed1c691a8bc2aa79458fd2f
umbc-dps <jgilless123@gmail.com>
2016-02-11 13:25:53 -0500

    Created draft carousel page for Navaal.

---
+++ Engineering Management/carousel.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+<title>Engineering Management at UMBC</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="Our Engineering program combines a practical business approach with an in-depth technical concentration and emphasizes how to lead people and manage complex projects." />
+
+
+<!-- Twitter Card data -->
+<meta name="twitter:card" value="summary">
+<meta name="twitter:site" content="@UMBC_DPS">
+
+<!-- Open Graph data -->
+<meta property="og:title" content="Engineering Management at UMBC" />
+<meta property="og:type" content="article" />
+<meta property="og:url" content="http://www.umbc.edu/engm" />
+<meta property="og:image" content="http://www.umbc.edu/engm/images/bk.jpg" />
+<meta property="og:description" content="Our Engineering program combines a practical business approach with an in-depth technical concentration and emphasizes how to lead people and manage complex projects." />
+
+
+
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+  <link href='css/magnific-popup.css' rel='stylesheet'>
+  <link href="css/styles.css" rel="stylesheet">
+  <script src="js/modernizr.custom.js"></script>
+  <?php # include("includes/tracking.php"); ?>
+  <link href="images/favicon.ico" rel="shortcut icon">
+  <link href="images/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+  <link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+  <link href='css/magnific-popup.css' rel='stylesheet'>
+  
+</head>
+
+<body>
+
+
+  <div id="skip-content">
+    <a href="#main-content">Skip to Main Content</a>
+  </div>
+
+<!-- main container -->
+<div id="container-header-background">
+
+<!-- umbc header -->
+<div id="container-header">
+ <?php include("includes/umbc-header.php"); ?>
+</div>
+<!-- END umbc header -->
+
+<!-- gold toolbar -->
+<div id="site-menu-background">
+ <?php include("includes/menu.php"); ?>
+</div>
+<!-- END gold toolbar -->
+
+</div>
+  
+  
+<!-- BEGIN MAIN -->
+
+
+
+<!-- Page Header -->
+<h3>Draft</h3>
+ <?php include("includes/header.php"); ?>
+ 
+<!-- END Page Header -->
+
+<div class="eleven columns">
+  <div class="cbp-fwslider" id="cbp-fwslider">
+    <ul>
+
+<!-- SLIDE ONE -->
+<li id="slide1">
+
+<a href="https://highpoint-prd.ps.umbc.edu/app/catalog/classSearch" target="_blank"><img alt="img01" src="images/6.png"></a>
+
+<div class="carousel-caption-right wordwrap">
+<h3>Register for Spring 2016</h3>
+<small>Spring 2016 semester begins <br>January 25.</small>  
+          
+<p><a href="https://highpoint-prd.ps.umbc.edu/app/catalog/classSearch" target="_blank" class="web-seemore">View Classes</a></p></div>
+
+<p id="captionthree"><a href="https://highpoint-prd.ps.umbc.edu/app/catalog/classSearch" target="_blank">Register for Spring 2016</a></p>
+
+</li>
+<!-- END SLIDE ONE -->
+
+
+<!-- SLIDE TWO --> 
+<li id="slide2">
+
+<a href="https://www.youtube.com/watch?v=ICsFC-pRO78" class="video"><img alt="img01" src="images/4.png"></a>
+
+<div class="carousel-caption wordwrap">
+<h3>Advanced Project Management </h3>
+<small>Steve Edwards, CEO of Edwards Project Solutions, talks about how the course ties academic learning with practical real-world experience.</small>
+
+<p><a href="https://www.youtube.com/watch?v=ICsFC-pRO78" class="web-seemore video">Watch the Video</a></p>
+</div>
+
+<p id="captionone">
+<a href="https://www.youtube.com/watch?v=ICsFC-pRO78" class="video">Featured Course: Adv. Project Management</a></p>
+
+</li>
+<!-- END SLIDE TWO --> 
+
+
+<!-- SLIDE THREE --> 
+<li id="slide3">
+
+<a href="../openhouse" target="blank"><img src="images/8.png" alt="Open House"/></a>
+
+<div class="carousel-caption wordwrap">
+<h3>Professional Programs' Program Exploration Night</h3>
+<small>Join us on Tuesday, April 26 to hear from program directors, faculty, alumni, and "sample" the curriculum to find out "Why UMBC" is a fit for you.</small>
+
+<p><a href="../openhouse" role="button" class="web-seemore" target="_blank">RSVP Now</a></p></div>
+
+<p id="captionthree"><a href="../openhouse" class="video" target="blank">Professional Programs' Program Exploration Night</a></p>
+
+</li>
+<!-- END SLIDE THREE --> 
+      
+    </ul>
+
+    <div id="dotcontainer">
+    </div>
+  </div>
+
+
+<!-- START Content -->
+
+<h3>UMBC Engineering Management Graduate Programs</h3>
+
+      <p>The Engineering Management Program at UMBC offers a Master of Science in Engineering Management and a Certificate in Engineering Management. UMBC's program combines a practical business approach with an in-depth technical concentration and emphasizes how to lead people and manage complex projects. Our engineering management courses are developed and taught by industry experts and are designed to address real-world problems in the workplace.</p>
+      <p>The Engineering Management Program at UMBC is designed to help maximize technical and business skills and is ideally suited for engineers employed in technology-oriented enterprises or government programs.<br>
+      </p>
+      <h3>Why Engineering Management?</h3>
+
+    <ul type="square">
+        <li>The       global environment demands skilled engineers who understand the essential       principles of management and business.</li>
+        <li>Combining       technical and business skills gives engineers a competitive advantage.       Engineering management professionals are trained to understand not just       how a system works technically, but also how these systems fit in and       relate to others.</li>
+        <li>Organizations       seek engineering experts who can understand the complexity of       technological and business-related challenges and who can create solutions       to solve them.<br>
+    </li>
+    </ul>
+
+    <h3>Why UMBC?</h3>
+
+      <ul>
+        <li>UMBC's Engineering Management Graduate Program  is led by a distinguished faculty of practitioners in the field.</li>
+        <li>UMBC is classified by the Carnegie Foundation  as a Research University (High Research Activity).</li>
+        <li>For six years running (2009-2014), UMBC was  ranked #1 in the U.S. News and World Report's list of &quot;national  up-and-coming&quot; universities.</li>
+        <li>Classes are conveniently offered in the  evening on UMBC's main campus, located just five minutes from BWI Airport, with  easy access to I-95 and the 695 Beltway.
+<!-- END Content --></li>
+      </ul>
+</div>  
+  
+<!-- SIDEBAR -->
+ <?php include("includes/sidebar.php"); ?>
+<!-- END SIDEBAR -->
+<br>  
+<!-- Footer -->
+<footer class="clearfix" id="site-footer">
+
+<div class="full-width column add-bottom" style="display:none">
+  <h3>Engineering Management Job Outlook </h3>
+    <p>According to the <a href="http://www.bls.gov/ooh/management/architectural-and-engineering-managers.htm#tab-6" target="_blank">Bureau of Labor Statistics</a>, employment of architectural and engineering managers is projected to grow 7 percent from 2012 to 2022.<br>
+<br>
+ Because these jobs are highly desirable, candidates can expect very strong competition for openings. Those with technical knowledge, strong communication skills, and years of related work experience will likely be in the best position to become managers.<br>
+    </p></div>
+
+
+
+<br clear="all">
+  <?php include("includes/footer.php"); ?>
+
+<br>
+
+</footer>
+<!-- END footer -->
+
+</div>  
+
+<!-- END container -->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+<script src="js/scripts.js"></script> 
+<script src="js/jquery.cbpFWSlider.js"></script>
+<script src="js/jquery.magnific-popup.js"></script>
+<script src="js/popup.js"></script>
+
+<script>
+$( function() {
+    var isPaused = false;
+    $( '#cbp-fwslider' ).cbpFWSlider()
+      .on({
+        mouseenter: function() { isPaused = true; },
+        mouseleave: function() { isPaused = false; }
+                        });
+
+    setInterval(function() {
+        if (isPaused) return; // do nothing when paused
+
+        if(jQuery('.cbp-fwnext').css('display') != 'none')
+            jQuery('.cbp-fwnext').click();
+        else
+            jQuery('.cbp-fwdots span:first-child').click();
+    }, 5000);
+});
+</script>
+
+<script>
+$('#cbp-fwslider').on('click', function() {
+  ga('send', 'event', 'slider', 'slide', {'nonInteraction': 1});
+});
+$('#slide1').on('click', function() {
+  var slideID = document.getElementById('slide1');
+  var slideLink = slideID.getElementsByTagName('a')[0];
+  var slideURL = slideLink.href;
+  ga('send', 'event', 'slider', 'slide1', slideURL);
+});
+$('#slide2').on('click', function() {
+  var slideID = document.getElementById('slide2');
+  var slideLink = slideID.getElementsByTagName('a')[0];
+  var slideURL = slideLink.href;
+  ga('send', 'event', 'slider', 'slide2', slideURL);
+});
+$('#slide3').on('click', function() {
+  var slideID = document.getElementById('slide3');
+  var slideLink = slideID.getElementsByTagName('a')[0];
+  var slideURL = slideLink.href;
+  ga('send', 'event', 'slider', 'slide3', slideURL);
+});
+$('#slide4').on('click', function() {
+  var slideID = document.getElementById('slide4');
+  var slideLink = slideID.getElementsByTagName('a')[0];
+  var slideURL = slideLink.href;
+  ga('send', 'event', 'slider', 'slide4', slideURL);
+});
+
+
+$(".cbp-fwnext").on('hover', function() {
+  ga('send', 'event', 'slider', 'button', 'next');
+});
+$(".cbp-fwprev").on('hover', function() {
+  ga('send', 'event', 'slider', 'button', 'previous');
+});
+$(".cbp-fwdots").on('hover', function() {
+  ga('send', 'event', 'slider', 'button', 'dots');
+});
+</script>
+
+  
+  
+</body>
+</html>
+
