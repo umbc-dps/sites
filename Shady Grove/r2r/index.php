c0eb3b1bb40d77300054224665f84ee755e6620e
umbc-dps <jgilless123@gmail.com>
2016-03-21 15:27:50 -0400

    Added R2R page.

---
+++ Shady Grove/r2r/index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>UMBC-Shady Grove Open House</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="Please follow the following steps to order your regalia and have it delivered to Shady Grove for pick-up." />
+
+<!-- Twitter Card data -->
+<meta name="twitter:card" value="summary">
+<meta name="twitter:site" content="@UatShadyGrove">
+
+<!-- Open Graph data -->
+<meta property="og:title" content="UMBC-Shady Grove Open House" />
+<meta property="og:type" content="article" />
+<meta property="og:url" content="http://www.umbc.edu/shadygrove/regalia" />
+<meta property="og:image" content="http://www.umbc.edu/shadygrove/images/commencement.png" />
+<meta property="og:description" content="Please follow the following steps to order your regalia and have it delivered to Shady Grove for pick-up." />
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+<link href="../css/styles.css" rel="stylesheet">
+<link href="css/accordion.css" rel="stylesheet">
+<link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'>
+
+<script src="../js/modernizr.custom.js"></script>
+
+
+
+<link rel="shortcut icon" href="../images/favicon.ico">
+<link rel="apple-touch-icon" href="../images/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../images/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../images/apple-touch-icon-114x114.png">
+
+<style>
+#mobile-site-menu-expander{display:none}
+</style>
+</head>
+
+
+<body>
+  <div id="skip-content">
+    <a href="#main-content">Skip to Main Content</a>
+  </div>
+
+<!-- main container -->
+<div id="container-header-background">
+
+<!-- umbc header -->
+<div id="container-header">
+ <?php include("../includes/umbc-header.php"); ?>
+</div>
+<!-- END umbc header -->
+
+<!-- gold toolbar -->
+<div id="site-menu-background">
+
+</div>
+<!-- END gold toolbar -->
+
+</div>
+
+<!-- BEGIN MAIN -->
+
+<!-- Page Header -->
+  <div class="container column">
+    <div class="sixteen columns">      
+
+<!-- END Page Header -->
+
+
+  
+<!-- START Content -->
+<br>
+
+<h3>
+	<a href="index">
+    	<img src="images/header.png" alt="Open House" style="width:100%">
+	</a>
+</h3>
+
+<h3>Program Description</h3>
+<p>
+Raptor to Retriever (R2R) is a transfer access program designed to streamline the transfer process between MC and UMBC-Shady Grove.  The program offers students a structured pathway for attaining an associate degree and a baccalaureate degree.
+</p>
+
+<h3>Admission Requirements</h3>
+<ul>
+	<li>Completed Application (Fall deadline: June 15; Spring deadline: November 15)</li>
+	<li>Minimum 2.0 MC GPA with an indicated interest in one of UMBC’s four undergraduate programs offered at USG: history, political science, psychology, and/or social work</li>
+	<li> Earned 12-36 credits by the end of the semester of application</li>
+	<li>Additional selection criteria (i.e. level of commitment to program and professional/academic/co-curricular involvement) will be used and garnered through the application process</li>
+</ul>  
+
+<p align="center">
+<a href="https://docs.google.com/forms/d/1PSRkMHXm5dVisMvNxJICeX_xsAYV-ZhJFyvCcLCwtHs/viewform?c=0&w=1&usp=mail_form_link" target="_blank" class="btn2">Fill Out Raptor to Retriever (R2R) Application</a>
+</p>
+
+
+<h3>Program Benefits</h3>
+<p>Upon acceptance into one of UMBC's programs at USG, students who complete all program requirements will receive the following benefits:</p>
+
+<ul>
+	<li>UMBC admissions application fee waiver</li>
+	<li>Priority UMBC-Shady Grove new student orientation program registration</li>
+	<li>Priority UMBC-Shady Grove pre-orientation advising and first semester course registration</li>
+	<li>Membership in the R2R Peer Network </li>
+	<li>Purple and gold graduation cords to wear at UMBC graduation </li>
+	<li>Student-focused and success-driven programming and applied learning opportunities</li>
+</ul>
+
+<p><em><strong>Note:</strong> Students must adhere to all UMBC admissions procedures and UMBC-Shady Grove program specific requirements.</em></p>
+
+
+<h3>Frequently Asked Question</h3>
+<h4>Who is eligible for R2R?</h4>
+<p>Any MC student who has earned between 12-36 credits and is interested in one of UMBC’s four undergraduate programs offered at USG: history, political science, psychology, and/or social work.</p>
+
+<h4>What are the requirements for successful completion of R2R?</h4>
+<ul>
+	<li>Signed and completed student-specific Success Agreement (SA)</li>
+	<li>Completion of required modules, events, and trainings</li>
+		<ul>
+	  		<li>Semesterly advising sessions with MC and UMBC representatives</li>
+	  		<li>Fall Retreat</li>
+	  		<li>Three workshops (at least one must be USG-offered)</li>
+		</ul>
+	<li> Completion of an Associate Degree from MC with a minimum cumulative GPA of 2.0</li>
+</ul>
+
+<h4>How do I get more information about UMBC-Shady Grove undergraduate programs?</h4>
+<ul>
+	<li>
+	<a href="../history">History</a> - 
+	Dr. Andrew Nolan, History Program Director, <a href="mailto:nolan@umbc.edu">nolan@umbc.edu</a>
+	</li>    	    
+	   
+	<li>
+	<a href="../politicalscience">Political Science</a> - 
+	Dr. Sunil Dasgupta, Political Science Program Director, <a href="mailto:sunil@umbc.edu">sunil@umbc.edu</a>
+	</li>
+	    
+	<li>
+	<a href="../psychology">Psychology</a> - 
+	Dr. Diane Alonso, Psychology Program Director, <a href="mailto:dalonso@umbc.edu">dalonso@umbc.edu</a> 
+	</li>	    
+
+	<li>
+	<a href="../socialwork">Social Work</a> - 
+	Mrs. Katie Morris, LCSW-C, Social Work Program Director, <a href="mailto:kath@umbc.edu">kath@umbc.edu</a>
+	</li>
+</ul>
+
+<p>If you would like to visit with a program director and/or gain general information about UMBC-Shady Grove, consider attending an on-campus event!
+</p>
+
+<p align="center">
+<a href="http://www.umbc.edu/shadygrove/infosession/pd" target="_blank" class="btn2">RSVP to Meet a Program Director</a>
+</p>
+
+<h4>How do you apply?</h4>
+<p>Submit the <a href="https://docs.google.com/forms/d/1PSRkMHXm5dVisMvNxJICeX_xsAYV-ZhJFyvCcLCwtHs/viewform?c=0&w=1&usp=mail_form_link" target="_blank">R2R application</a> November 15 (spring semester) for priority consideration.
+</p>
+
+<h3>Contact Us/Request Info</h3>
+
+<ul>
+	<li>Chelsea S. Moyer - 
+  Undergraduate Program Coordinator, 
+  UMBC at The Universities at Shady Grove | 301-738-6184 - <a href="mailto:csmoyer@umbc.edu">csmoyer@umbc.edu</a>
+	</li>
+
+	<li>Tim H. Cox - Assistant Director of Advising and Student Success, UMBC at The Universities at Shady Grove | 
+  301-738-6281 - <a href="mailto:thcox@umbc.edu">thcox@umbc.edu</a>
+	</li>
+
+<li>Justin Edgar - Assistant Director of Transfer Access Programs, Universities at Shady Grove | 301-738-6071 - 
+  <a href="mailtp:jedgar@umd.edu">jedgar@umd.edu</a>
+</li>
+
+<li>Seth Marc Kamen, MBA - Articulation, Transfer and Academic Services Manager, Montgomery College | 240-567-4255 - <a href="mailto:seth.kamen@montgomerycollege.edu">seth.kamen@montgomerycollege.edu</a>
+</li>
+</ul>
+
+	    
+      <!-- END Content -->
+	    
+	    
+      <!-- SIDEBAR -->
+	    
+	    
+	    
+      <!-- END SIDEBAR -->
+      <!-- footer -->
+      <br clear="all">
+	    
+</p>
+<?php include("../includes/footer.php"); ?>
+<?php include("../includes/tracking.php"); ?>
+
+<br>
+    <!--END footer -->
+  </div>
+  <!-- container -->
+  
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+ 
+<script src="../js/scripts.js"></script> 
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<script>
+$( "#accordion" ).accordion({
+      heightStyle: "content",
+      active:false,
+      collapsible: true,
+      header:"div.accordionheader"
+    });
+    var hash = window.location.hash;
+    var anchor = $('a[href$="'+hash+'"]');
+    if (anchor.length > 0){
+        anchor.click();
+    }
+    $('h4').click(function() {
+	var linkText = $(this).text();
+	ga('send', 'event',  'accordion', 'click', linkText);
+});
+</script>   
+
+
+</body>
+</html>
