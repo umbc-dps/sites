c5f0c3e94c184fa0f8a9ece177616f0ce1faabf9
umbc-dps <jgilless123@gmail.com>
2016-04-21 12:59:10 -0400

    Added email php form test.

---
+++ Cyber/includes/program-info.php
@@ -0,0 +1,34 @@
+<table id="forminfo">
+  <tr>
+    <td>program</td>
+    <td>pctitle</td>
+    <td>pc</td>
+    <td>cc</td>
+    <td>infosession</td>
+    <td>Body</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>biotech</td>
+    <td>Person Name, Position</td>
+    <td>co7@umbc.edu</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>cyber</td>
+    <td>Lisa Gambino, Assistant Director</td>
+    <td>co7@umbc.edu</td>
+    <td></td>
+    <td>https://youtu.be/ecx3c_h-IgU</td>
+    <td>UMBC’s Cybersecurity program reflects the interdisciplinary nature of the modern cybersecurity environment offering students the necessary educational experience required for successful and effective cybersecurity practice in today’s workforce. Since our program blends both the technical and non-technical aspects of cybersecurity, our graduates share a common understanding of the operational cybersecurity environment and also have the opportunity to expand their knowledge on specific cybersecurity interests.</td>
+  </tr>
+</table>
